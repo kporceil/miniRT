718f069f1fcebacbe6b41b5c2854e00c2551c909
kporceil <163202211+kporceil@users.noreply.github.com>
2025-11-29 18:41:53 +0100

    feat: parsing utils

---
+++ srcs/scenes_parsing/parse_vector.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_vector.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kporceil <kporceil@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 19:06:47 by kporceil          #+#    #+#             */
+/*   Updated: 2025/11/28 19:07:15 by kporceil         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tuples.h"
+#include "libft.h"
+
+int	parse_vector(char *file, char **endptr, t_tuple *point)
+{
+	point->x = ft_strtod(file, endptr);
+	if (file == *endptr || **endptr != ',')
+		return (-1);
+	file = *endptr + 1;
+	point->y = ft_strtod(file, endptr);
+	if (file == *endptr || **endptr != ',')
+		return (-1);
+	file = *endptr + 1;
+	point->z = ft_strtod(file, endptr);
+	if (file == *endptr)
+		return (-1);
+	point->w = 0;
+	return (0);
+}
+
