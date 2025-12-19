8df96483af4c516383201c9b7a9b72179efa9275
kporceil <163202211+kporceil@users.noreply.github.com>
2025-12-19 17:16:22 +0100

    style: norm

--- srcs/scenes_parsing/parse_file.c
+++ srcs/scenes_parsing/parse_file.c
@@ -83,12 +83,12 @@ static int	convert_world_list(t_world *world)
 	}
 	if (world->objs_count != 0)
 	{
-	world->objs = malloc(sizeof(t_shape) * world->objs_count);
-	if (!world->objs)
-	{
-		free_world(world);
-		return (-1);
-	}
+		world->objs = malloc(sizeof(t_shape) * world->objs_count);
+		if (!world->objs)
+		{
+			free_world(world);
+			return (-1);
+		}
 	}
 	copy_list_in_array(world);
 	world->buf_inter = malloc(sizeof(t_inter)
